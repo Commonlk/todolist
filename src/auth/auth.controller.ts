08a3b245bfa2b3fe76615108a42f3690c4621030
Victor <vitinsilva0xd@gmail.com>
2022-03-08 12:20:51 -0300

    Add services for routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { AuthDto } from './dto';
+import { AuthService } from './auth.service';
+import { AuthDto, LoginDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('signup')
-  signup(@Body() dto: AuthDto) {}
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 
   @Post('signin')
-  signin(@Body() dto: AuthDto) {}
+  signin(@Body() dto: LoginDto) {
+    return this.authService.signin(dto);
+  }
 }
