afdba90224d3c134ef601fd232bc8dbe7971a1ac
Victor <vitinsilva0xd@gmail.com>
2022-03-09 11:37:52 -0300

    Add not empty decorator for name field

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @IsString()
   @IsOptional()
+  @IsNotEmpty()
   name?: string;
 
   @IsEmail()
