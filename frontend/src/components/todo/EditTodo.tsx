2b001496292df29c797b60199079abdcbf952fb7
Victor <vitinsilva0xd@gmail.com>
2022-03-15 13:58:57 -0300

    Add create and edit todo components

--- frontend/src/components/todo/EditTodo.tsx
+++ frontend/src/components/todo/EditTodo.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 
 const EditTodo = () => {
-  return <div>EditTodo</div>;
+  return (
+    <Container maxWidth="sm" sx={{ marginTop: 5 }}>
+      <Typography variant="h4" sx={{ fontSize: 16 }}>
+        Edit todo
+      </Typography>
+      <Box component="form" autoComplete="off" sx={{ marginTop: 2 }}>
+        <TextField fullWidth required id="title" label="Title" />
+        <TextField
+          sx={{ marginTop: 2 }}
+          fullWidth
+          multiline
+          maxRows={8}
+          minRows={5}
+          id="description"
+          label="Description"
+        />
+        <Box sx={{ display: "flex", justifyContent: "end", marginTop: 2 }}>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Important"
+            labelPlacement="start"
+          />
+        </Box>
+        <Button sx={{ marginTop: 2 }} fullWidth variant="contained">
+          Edit
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default EditTodo;
