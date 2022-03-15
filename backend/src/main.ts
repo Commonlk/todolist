87c5f420bf19d59626cf9f6434e41d7dfb0c856e
Victor <vitinsilva0xd@gmail.com>
2022-03-15 13:58:02 -0300

    Add cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,12 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    },
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
