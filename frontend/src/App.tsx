f1760669ccd1e18e0ee73a3c8a3472a21c737b35
Victor <vitinsilva0xd@gmail.com>
2022-03-15 12:00:21 -0300

    Add routes for signin,signup,login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,19 @@ import Header from "./components/header/Header";
 import CreateTodo from "./components/todo/CreateTodo";
 import EditTodo from "./components/todo/EditTodo";
 import TodoContainer from "./components/todo/TodoContainer";
+import Login from "./components/user/Login";
+import Signin from "./components/user/Signin";
+import Signup from "./components/user/Signup";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Routes>
-        <Route path="/" element={<TodoContainer />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/todos" element={<TodoContainer />} />
         <Route path="/create" element={<CreateTodo />} />
         <Route path="/edit" element={<EditTodo />} />
       </Routes>
