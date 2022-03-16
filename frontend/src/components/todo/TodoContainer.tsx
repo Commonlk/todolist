5664fbb132514c3a75f462ca754a0cbb698ffe77
Victor <vitinsilva0xd@gmail.com>
2022-03-16 14:16:30 -0300

    Add db todos

--- frontend/src/components/todo/TodoContainer.tsx
+++ frontend/src/components/todo/TodoContainer.tsx
@@ -1,72 +1,31 @@
 import { Container, Grid, Typography } from "@mui/material";
+import { Todo } from "../../models/todo";
 import TodoCard from "./TodoCard";
 
-const DUMMY_TODOS = [
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-  {
-    title: "Test todo",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, perferendis cumque. Earum est doloribus aperiam ducimus natus totam ullam pariatur laboriosam recusandae. Similique voluptatum amet ipsum aliquid recusandae. Temporibus, voluptas?",
-    important: false,
-    createdAt: "14/03/2022",
-  },
-];
+interface Props {
+  todos: Todo[];
+  deleteTodo: (id: string) => void;
+  updateTodo: (id: string, todo: Todo) => void;
+}
 
-const TodoContainer = () => {
+const TodoContainer = ({ todos, deleteTodo, updateTodo }: Props) => {
   return (
     <Container maxWidth="sm" sx={{ marginTop: 4 }}>
       <Typography variant="h3" fontSize={16}>
         Todos
       </Typography>
       <Grid container sx={{ marginTop: 2 }} spacing={2}>
-        {DUMMY_TODOS.map((todo, index) => (
-          <Grid key={index} item sx={{ minWidth: "100%" }}>
+        {todos.map(todo => (
+          <Grid key={todo.id} item sx={{ minWidth: "100%" }}>
             <TodoCard
+              id={todo.id}
               title={todo.title}
               description={todo.description}
               important={todo.important}
               createdAt={todo.createdAt}
+              completed={todo.completed}
+              deleteTodo={deleteTodo}
+              updateTodo={updateTodo}
             />
           </Grid>
         ))}
