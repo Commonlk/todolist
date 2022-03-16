9521e93eb7bd06fd9d87fbc163ec53b1e6d816e6
Victor <vitinsilva0xd@gmail.com>
2022-03-16 14:17:02 -0300

    Add function handlers for todo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import agent from "./api/agent";
 import Header from "./components/header/Header";
 import CreateTodo from "./components/todo/CreateTodo";
 import EditTodo from "./components/todo/EditTodo";
@@ -6,8 +8,42 @@ import TodoContainer from "./components/todo/TodoContainer";
 import Login from "./components/user/Login";
 import Signin from "./components/user/Signin";
 import Signup from "./components/user/Signup";
+import { Todo } from "./models/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const todos = await agent.Todos.todoList();
+
+      setTodos(todos);
+    };
+
+    getTodos();
+  }, []);
+
+  const handleTodo = (todo: Todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const deleteTodo = (id: string) => {
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+    setTodos([...updatedTodos]);
+  };
+
+  const updateTodo = (id: string, updatedTodo: Todo) => {
+    const updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        console.log({ todo });
+        todo = { ...todo, ...updatedTodo };
+        console.log({ todo });
+      }
+      return todo;
+    });
+    setTodos([...updatedTodos]);
+  };
+
   return (
     <div className="app">
       <Header />
@@ -15,9 +51,24 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/todos" element={<TodoContainer />} />
-        <Route path="/create" element={<CreateTodo />} />
-        <Route path="/edit" element={<EditTodo />} />
+        <Route
+          path="/todos"
+          element={
+            <TodoContainer
+              todos={todos}
+              deleteTodo={deleteTodo}
+              updateTodo={updateTodo}
+            />
+          }
+        />
+        <Route
+          path="/create"
+          element={<CreateTodo handleTodo={handleTodo} />}
+        />
+        <Route
+          path="/edit/:id"
+          element={<EditTodo todos={todos} updateTodo={updateTodo} />}
+        />
       </Routes>
     </div>
   );
